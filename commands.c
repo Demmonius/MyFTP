3294c1206aed489c37152f60d7ba669928f0769b
Hugo MARTIN <hugo.martin@epitech.eu>
2018-04-18 13:03:20 +0000

    New commands gestion commands_func is an array of func commands_name is an array of appropriate name function Compare input command with the commands_name and then, execute commands_func corresponding

---
+++ commands.c
@@ -0,0 +1,32 @@
+/*
+** EPITECH PROJECT, 2018
+** MyFTP
+** File description:
+** Command gestion C file
+*/
+
+#include "server.h"
+
+void	commands_ls(t_client *client)
+{
+	printf("LS started\n");
+}
+
+void (*const commands_func[])(t_client *) = {
+	commands_ls
+};
+
+const char commands_name[][64] = {
+	"ls"
+};
+
+int manage_commands(char *command, t_client *client)
+{
+	printf("Command: %s\n", command);
+	for (int i = 0; i < LEN_FUNCS; i++) {
+		printf("%s => %s\n", command, commands_name[i]);
+		if (strcmp(commands_name[i], command) == 0)
+			commands_func[i](client);
+	}
+	return (0);
+}
